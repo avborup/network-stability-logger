98987278b25f1d20ed6b678c27a6a3b40d6a2033
Adrian Borup <avborup@gmail.com>
2021-10-19 11:44:50 +0200

    Make max bar height fixed

--- src/ui.rs
+++ src/ui.rs
@@ -43,7 +43,7 @@ impl<W: Write> Ui<W> {
         return Self {
             buffer,
             bar_chart_meta_data: BarChartMetaData {
-                max_bar_value: BAR_CHART_RED_START + 2.0,
+                max_bar_value: BAR_CHART_RED_START * 1.25,
                 min_bar_value: 0.0,
                 max_seen_value: None,
                 min_seen_value: None,
@@ -107,13 +107,6 @@ impl<W: Write> Ui<W> {
             .min_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal))
             .unwrap();
 
-        if max > bcmd.max_bar_value {
-            bcmd.max_bar_value = max;
-        }
-        if min < bcmd.min_bar_value {
-            bcmd.min_bar_value = min;
-        }
-
         match bcmd.max_seen_value {
             Some(m) if max > m => bcmd.max_seen_value = Some(max),
             None => bcmd.max_seen_value = Some(max),
@@ -158,6 +151,13 @@ impl<W: Write> Ui<W> {
                 Print(format!("{} ms", y)),
             )?;
         }
+        queue!(
+            self.buffer,
+            MoveTo(area.left(), 0),
+            Print("⡀".repeat((area.right() - area.left() - 1) as usize)),
+            MoveTo(area.left(), 1),
+            Print(format!("{}+ ms", max.round())),
+        )?;
 
         for (i, datapoint) in shown_datapoints.iter().rev().enumerate() {
             queue!(self.buffer, SetForegroundColor(datapoint.color()))?;
@@ -284,6 +284,9 @@ impl Rect {
     }
 }
 
+/// Maps a value `n` from the range `a..b` to a new range `c..d`. If the n falls
+/// outside of `a..b`, the result is clamped to be within `c..d`.
 fn map_range(value: f64, old_min: f64, old_max: f64, new_min: f64, new_max: f64) -> f64 {
-    (value - old_min) / (old_max - old_min) * (new_max - new_min) + new_min
+    ((value - old_min) / (old_max - old_min) * (new_max - new_min) + new_min)
+        .clamp(new_min, new_max)
 }
